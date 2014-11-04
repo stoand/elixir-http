70f1d9554da6e7c1b0bc7375998826f89deb8c84
Arubaruba <shadowstep7@gmail.com>
2014-11-04 12:23:46 +0200

    Request module fully documented

--- lib/request.ex
+++ lib/request.ex
@@ -1,13 +1,37 @@
 defmodule Request do
 
-  def data(connection, loop \\ true, previous_bytes \\ []) do
+  @doc """
+  Recieve data from a Connection\n
+
+  ### Recieve data untill the connection closes
+
+      data = Request.data(connection)
+
+  ### Recieve data once
+
+      data = Request.data(connection, :once)
+  """
+  def data(connection, loop \\ :until_closed, previous_bytes \\ []) do
     case :gen_tcp.recv(connection, 0) do
-      {:ok, bytes} when loop == false -> {:ok, IO.iodata_to_binary(bytes)}
+      {:ok, bytes} when loop == :once -> {:ok, IO.iodata_to_binary(bytes)}
       {:ok, bytes} -> data(connection, true, [previous_bytes, bytes])
       {:error, :closed} -> {:ok, IO.iodata_to_binary(previous_bytes)}
     end
   end
 
+  @doc """
+  Recieves data once and parses basic header information and request fields\n
+
+  ### Example:
+      path = header(connection).path
+
+  Name | Description
+  --- | ---
+  method | GET, POST, PUT ...
+  path | The url without the query ex: http://localhost/one/two/three?test=1 -> /one/two/three
+  query | The unparsed query string ex: http://localhost/one/two/three?test=1 -> test=1
+  fields | Accept, Cookies ...
+  """
   def header(connection) do
     {:ok, header} = data(connection, false) # Receive only once
     [basic_info | encoded_fields] = String.split(to_string(header), "\r\n")
@@ -24,14 +48,10 @@ defmodule Request do
       end
     end)
 
-
-    Map.merge(
-      %{:fields => fields, :method => method},
-      case String.split(url, "?") do
-        [path, query_params] -> %{:path => path, :get_params => parse_params(query_params)}
-        [path] -> %{:path => path, :get_params => %{}}
-      end
-    )
+    case String.split(url, "?") do
+      [path, query] -> %{:method => method, :fields => fields, :path => path, :query => query}
+      [path] -> %{:method => method, :fields => fields, :path => path, :query => nil}
+    end
   end
   @doc ~S"""
   Parses GET or POST parameters from a String into a Map\n
@@ -41,6 +61,8 @@ defmodule Request do
       iex> Request.parse_params "a=0&b[]=1&b[]=2"
       %{"a" => "0", "b" => ["2", "1"]}
   """ 
+  def parse_params(nil), do: %{}
+  def parse_params(""), do: %{}
   def parse_params(encoded_params) do
     key_value_pairs = String.split(encoded_params,"&")
     Enum.reduce(key_value_pairs, %{}, fn(key_value_pair, map) ->
